624f9c33ca2e5c64b106d4d980a4a862ff587a6b
Francis DiTraglia <fditra@sas.upenn.edu>
2016-08-21 21:47:33 -0400

    Start work in SummarizeAndPlot.R
    This file will contain functions for plotting and summarizing results.
    This commit moves get_estimates from IdentifiedSet.R to
    SummarizeAndPlot.R since it makes more sense for it to be there. It also
    adds a function get_HPDI for calculating the posterior median along with
    a 90% HPDI using the package coda.

--- R/IdentifiedSet.R
+++ R/IdentifiedSet.R
@@ -1,28 +1,3 @@
-get_estimates <- function(y_name, T_name, z_name, data, controls = NULL,
-                          robust = FALSE) {
-  first_stage <- reformulate(c(z_name, controls), response = NULL)
-  second_stage <- reformulate(c(T_name, controls), response = y_name)
-  OLS <- lm(second_stage, data)
-  IV <-  AER::ivreg(second_stage, first_stage, data)
-
-  b_OLS <- coef(OLS)[T_name]
-  b_IV <- coef(IV)[T_name]
-
-  if (robust) {
-    se_OLS <- sqrt(diag(sandwich::vcovHC(OLS, type = 'HC0')))[T_name]
-    se_IV <- sqrt(diag(sandwich::vcovHC(IV, type = 'HC0')))[T_name]
-  } else {
-    se_OLS <- sqrt(diag(vcov(OLS)))[T_name]
-    se_IV <- sqrt(diag(vcov(IV)))[T_name]
-  }
-
-  list(n = nrow(data),
-       b_OLS = b_OLS,
-       se_OLS = se_OLS,
-       b_IV = b_IV,
-       se_IV = se_IV)
-}
-
 get_observables <- function(y_name, T_name, z_name, data, controls = NULL) {
 
   # Project out control regressors if present
---
+++ R/SummarizeAndPlot.R
@@ -0,0 +1,31 @@
+get_estimates <- function(y_name, T_name, z_name, data, controls = NULL,
+                          robust = FALSE) {
+  first_stage <- reformulate(c(z_name, controls), response = NULL)
+  second_stage <- reformulate(c(T_name, controls), response = y_name)
+  OLS <- lm(second_stage, data)
+  IV <-  AER::ivreg(second_stage, first_stage, data)
+
+  b_OLS <- coef(OLS)[T_name]
+  b_IV <- coef(IV)[T_name]
+
+  if (robust) {
+    se_OLS <- sqrt(diag(sandwich::vcovHC(OLS, type = 'HC0')))[T_name]
+    se_IV <- sqrt(diag(sandwich::vcovHC(IV, type = 'HC0')))[T_name]
+  } else {
+    se_OLS <- sqrt(diag(vcov(OLS)))[T_name]
+    se_IV <- sqrt(diag(vcov(IV)))[T_name]
+  }
+
+  list(n = nrow(data),
+       b_OLS = b_OLS,
+       se_OLS = se_OLS,
+       b_IV = b_IV,
+       se_IV = se_IV)
+}
+
+get_HPDI <- function(draws, level = 0.9) {
+  interval <- coda::HPDinterval(coda::as.mcmc(draws), level)
+  lower <- interval[[1]]
+  upper<- interval[[2]]
+  return(data.frame(lower = lower, median = median(draws), upper = upper))
+}
