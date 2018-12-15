2bae79f99b4036b899bb26ffcfecc8f0b84c7f1f
emallickhossain <emallickhossain@gmail.com>
2018-12-14 19:48:23 -0500

    add psi bounds code

---
+++ R/Psi_bounds.R
@@ -0,0 +1,45 @@
+#' Computes the upper bound of psi for binary data
+#'
+#' @param draws data.frame of observables of simulated data
+#' @param p Treatment probability from binary data
+#' @param kappa Vector of kappa, NOTE: kappa_tilde in the paper
+#'
+#' @return Vector of upper bounds for psi
+#' @export
+#'
+get_psi_upper <- function(draws, p, kappa) {
+  ans <- (-draws$s2_T * (1 - kappa)) / max(p, 1 - p)
+  return(ans)
+}
+
+#' Computes the lower bound of psi for binary data
+#'
+#' @param draws data.frame of observables of simulated data
+#' @param p Treatment probability from binary data
+#' @param kappa Vector of kappa, NOTE: kappa_tilde in the paper
+#'
+#' @return Vector of lower bounds for psi
+#' @export
+#'
+get_psi_lower <- function(draws, p, kappa) {
+  m <- max((1 - p) * (2 * p - 2), p * (1 - 2 * p))
+  ind <- (draws$s2_T * (1 - kappa) <= m)
+  psi1 <- (-draws$s2_T * (1 - kappa)) / min(p, 1 - p)
+  psi2 <- 2 * sqrt(p * (1 - p) - draws$s2_T * (1 - kappa)) - 1
+  psi_lower <- psi1 * ind + psi2 * (1 - ind)
+  return(psi_lower)
+}
+
+#' Computes L, lower bound for kappa_tilde in paper
+#'
+#' @param draws data.frame of observables of simulated data
+#'
+#' @return Vector of L values
+#' @export
+#'
+get_L <- function(draws) {
+  num <- with(draws, r_Ty ^ 2 + r_Tz ^ 2 - 2 * r_Ty * r_Tz * r_zy)
+  denom <- with(draws, 1 - r_zy ^ 2)
+  L <- num / denom
+  return(L)
+}
